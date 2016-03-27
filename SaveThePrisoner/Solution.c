f998274185a57bcca92a585996a3341eb1c75178
idkwhatamidoing <chia96@live.com>
2016-03-27 22:12:52 +0800

    Update Solution.c
    I'm crying

--- SaveThePrisoner/Solution.c
+++ SaveThePrisoner/Solution.c
@@ -7,21 +7,21 @@ int main() {
     scanf("%d",&T);
 
     while(T!=0){
+        
         scanf("%lld %lld %lld",&N, &M, &S);
-
+        
         if(M>=N){
-            if(M%N==0) a=M/N-1;
+            if (M==N) a=M;
+            else if(M%N==0) a=M/N-1;
             else a=S+(M%N-1);
         }
-
         else a=S+(M-1);
 
-        if (a>N)
-            a-=N;
+        if (a>N) a-=N;
 
         T--;
         printf("%lld\n",a);
     }
-
     return 0;
 }
+

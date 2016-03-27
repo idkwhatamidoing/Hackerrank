d640b22433c9239efdc07a35e031dff3a1c5015c
idkwhatamidoing <chia96@live.com>
2016-03-28 00:19:19 +0800

    Update Solution.java
    runtime too long ;___; can't have too much for loops

--- SherlockAndTheBeast/Solution.java
+++ SherlockAndTheBeast/Solution.java
@@ -1,8 +1,5 @@
 import java.io.*;
 import java.util.*;
-import java.text.*;
-import java.math.*;
-import java.util.regex.*;
 
 public class Solution {
 
@@ -19,17 +16,13 @@ public class Solution {
             int n = in.nextInt();
             
             if(n==1 || n==2) d="-1";
-            
+            else if (n<10 && n%3==1) d="-1";
             else{
                 f=n/3;
-                r=n%3; 
+                r=n%3;
                 if(r!=0){
                     f-=1;
-                    r+=3;
-                    while(r%5!=0){
-                        f-=1;
-                        r+=3;
-                    }
+                    while((r+=3)%5!=0) f-=1;
                 }
                 for(int a1=0; a1<f; a1++) d+="555";
                 for(int a2=0; a2<(r/5); a2++)  d+="33333";

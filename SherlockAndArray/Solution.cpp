6f7f53bcd239c3538cf1ad92d993ce80b0c6c5e1
idkwhatamidoing <chia96@live.com>
2016-03-29 21:52:40 +0800

    Update Solution.cpp

--- SherlockAndArray/Solution.cpp
+++ SherlockAndArray/Solution.cpp
@@ -2,13 +2,13 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main(){
     
     bool exist=false;
     int T, N, I;
     cin>>T;
     
-    while (T!=0){
+    while(T!=0){
        
         cin>>N;
         int A[N],j=N-1,i=0,b=0,c=j;
@@ -16,7 +16,7 @@ int main() {
         for(int a0=0; a0<N; a0++) cin>>A[a0];
         
         if(N==1) exist=true;
-        else if(N==2);
+        else if(N==2) exist=false;
             
         for(int a1=0; a1<N-2; a1++){
             
@@ -24,7 +24,7 @@ int main() {
            else if(A[i]<A[j]) A[i]+=A[++b];
            else{
                    if(N%2==0 && (c-1)-(b+1)==1){exist=false; break;}
-                   else if(N%2==0 && (c-1)-(b-1)==0){ exist=true; break;}
+                   else if(N%2==0 && (c-1)-(b-1)==0){exist=true; break;}
                
                     A[i]+=A[++b];
                     A[j]+=A[--c];

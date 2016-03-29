022ff4a254af6caf0deeb969dc6ff9fbfd384fae
idkwhatamidoing <chia96@live.com>
2016-03-29 21:35:09 +0800

    Update Solution.cpp
    Just keep trying... keep trying  ;____;

--- SherlockAndArray/Solution.cpp
+++ SherlockAndArray/Solution.cpp
@@ -9,27 +9,29 @@ int main() {
     cin>>T;
     
     while (T!=0){
+       
         cin>>N;
         int A[N],j=N-1,i=0,b=0,c=j;
+        
         for(int a0=0; a0<N; a0++) cin>>A[a0];
         
         if(N==1) exist=true;
         else if(N==2);
             
-        for(int a1=0; a1<(N-2); a1++){
+        for(int a1=0; a1<N-2; a1++){
+            
            if(A[i]>A[j]) A[j]+=A[--c];
            else if(A[i]<A[j]) A[i]+=A[++b];
            else{
+                   if(N%2==0 && (c-1)-(b+1)==1){exist=false; break;}
+                   else if(N%2==0 && (c-1)-(b-1)==0){ exist=true; break;}
+               
                     A[i]+=A[++b];
                     A[j]+=A[--c];
-                    if(A[i]<A[j] || A[i]>A[j]) exist=false;
-                    else exist=true;
+               
+                   if(A[i]<A[j] || A[i]>A[j]) exist=false;
+                   else exist=true;
               }
-        }
-        
-        if(N%2==0 && N!=4){
-               A[j]-=A[c];
-               if(A[i]!=A[j]) exist=false;
         }
         
         if (exist==true) cout<<"YES"<<endl;

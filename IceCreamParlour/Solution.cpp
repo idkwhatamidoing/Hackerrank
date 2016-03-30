8ec9a97783fa19e6e5835d15ebfb81be11765c43
idkwhatamidoing <chia96@live.com>
2016-03-31 00:29:11 +0800

    Update Solution.cpp
    Parlour, parlor..........

--- IceCreamParlour/Solution.cpp
+++ IceCreamParlour/Solution.cpp
@@ -3,26 +3,22 @@
 using namespace std;
 
 int main(){
-    int T,M,N,c[N],index[N],list[N];
+    int T,M,N;
     cin>>T;
-   
     while(T>0){
-        
         cin>>M;
         cin>>N;
-        
+        int index[N],list[N],c[N];
         for(int a1=0, a0=0; a0<N; a0++){
             cin>>c[a0];
-            
             if(c[a0]<M){
                 index[a1]=a0+1;
                 list[a1]=c[a0];
-                
                 for(int a2=0; a2<a1; a2++){
-                     if(list[a0]+list[a1]==M && a0!=a2) { 
-                          cout<<index[a2]<<" "<<a0+1<<endl;
-                          break;
-                     } 
+                    if(list[a2]+list[a1]==M && a0!=a2) { 
+                         cout<<index[a2]<<" "<<a0+1<<endl;
+                         break;
+                    } 
                 }
                 a1++;   
             }

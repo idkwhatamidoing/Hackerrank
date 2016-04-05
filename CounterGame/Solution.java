4d68b3e5fe550a02eef1973700a33a0f3ba3e87b
idkwhatamidoing <chia96@live.com>
2016-04-05 16:16:43 +0800

    Update Solution.java
    still having problems with big integer and oop

--- CounterGame/Solution.java
+++ CounterGame/Solution.java
@@ -1,34 +1,38 @@
 import java.io.*;
 import java.util.*;
+import java.math.*;
 
-public class Solution {
+public class Solution{
     
-    static void turns(int a){
+    public static void turns(int a){
         if(a%2==0) System.out.println("Richard");
         else System.out.println("Louise");
     }
-    
-    
-    public static void main(String[] args) {
+ 
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int winner=1; 
-        long counter;
+        BigInteger N, n, two;
+        two = new BigInteger("2");
         
         int T=sc.nextInt();
         for(int i0=0; i0<T; i0++){
-            long N=sc.nextInt();
-            long n=N;
-            while(N!=1){
+            N = new BigInteger(sc.nextBigInteger());
+            n = new BigInteger(bigIntN);
+            
+            while(N!=BigInteger.ONE){
                 String ctr="1";
+               
                 BigInteger.toBinaryString(n);
                 if(BigInteger.bitCount(n)>1){
                     ctr=repeat("0",BigInteger.length()-2);
                     counter=N-BigInteger.parseInt(ctr,2);
                 }
-                else counter=N/2;
+                else counter=N.divide(two);
                 winner++;
             }
             turns(winner);
         }
     }
+    
 }

a8a21a9969bda51270b0e243d6e9864bff7d6527
idkwhatamidoing <chia96@live.com>
2016-04-19 10:00:27 +0800

    Update Solution.rb
    something is still wrong

--- SteppingStonesGame/Solution.rb
+++ SteppingStonesGame/Solution.rb
@@ -2,11 +2,13 @@ t=gets.to_i
 
 while t!= 0
     n=gets.to_i
-    x=(1-(Math.sqrt(1-8*n)))/2
-    if(x.is_a?Integer==true) 
-        puts "Go On Bob\n"
+    x=(1-(Math.sqrt(1+8*n)))/2 
+    if(x.floor==x)
+        puts "Go On Bob #{x.abs.floor}\n"
+        break
     else
         puts "Better Luck Next Time\n"
+        break
     t-=1
     end
 end
